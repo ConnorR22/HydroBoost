feec1b7ebf8c5f68b890dd4540faabb28b8dc518
Ben Hickman <bn727034@dal.ca>
2023-03-05 18:13:37 -0400

    Created basic project structure.

---
+++ app/src/main/java/com/example/hydroboost/ui/home/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hydroboost.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hydroboost.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
