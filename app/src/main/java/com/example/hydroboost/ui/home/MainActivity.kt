948602a09e0222be41201bfac00b368074189330
Ben Hickman <bn727034@dal.ca>
2023-03-05 20:57:57 -0400

    Included loading screen that transitions to HomeActivity

--- app/src/main/java/com/example/hydroboost/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/hydroboost/ui/home/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.hydroboost.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.hydroboost.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide() //Remove the action bar for loading screen
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val switchToHome = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(switchToHome)
+            finish()
+        }, 1500)
     }
 }
