f758ac144aa49e2d8b682ee76d58c9446855a42d
Ben Hickman <bn727034@dal.ca>
2023-03-21 20:47:35 -0300

    Created a simple nav bar to switch between fragments.

--- app/src/main/java/com/example/hydroboost/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/hydroboost/ui/home/HomeActivity.kt
@@ -2,11 +2,44 @@ package com.example.hydroboost.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.hydroboost.R
+import com.example.hydroboost.databinding.ActivityHomeBinding
+import com.example.hydroboost.ui.history.HistoryFragment
+import com.example.hydroboost.ui.metrics.MetricsFragment
+import com.example.hydroboost.ui.notifications.NotificationsFragment
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var homeBinding : ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        homeBinding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
+
+        homeBinding.navBar.setOnItemSelectedListener {
+            var item = it.itemId
+
+            if (item == R.id.homeIcon)
+                updateFrameLayoutFromNavBar(HomeFragment())
+            else if (item == R.id.notificationsIcon)
+                updateFrameLayoutFromNavBar(NotificationsFragment())
+            else if (item == R.id.metricsIcon)
+                updateFrameLayoutFromNavBar(MetricsFragment())
+            else if (item == R.id.historyIcon)
+                updateFrameLayoutFromNavBar(HistoryFragment())
+
+            true
+        }
+
+        updateFrameLayoutFromNavBar(HomeFragment())
+    }
+
+    private fun updateFrameLayoutFromNavBar(fragment : Fragment) {
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        transaction.replace(R.id.frameLayout, fragment)
+        transaction.commit()
     }
 }
