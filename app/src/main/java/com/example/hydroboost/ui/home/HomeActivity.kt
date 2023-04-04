dad48f65613dfe93a74dab23874bfc70bed18291
Emily Tan <emilytan@dal.ca>
2023-04-04 17:57:19 -0300

    notification settings page ui complete, logic is a WIP

--- app/src/main/java/com/example/hydroboost/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/hydroboost/ui/home/HomeActivity.kt
@@ -1,12 +1,22 @@
 package com.example.hydroboost.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.hydroboost.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
+        // TODO must delete when navigation bar is fully implemented
+        val temp = findViewById<Button>(R.id.button)
+        temp.setOnClickListener {
+            val i = Intent(this@HomeActivity, NotificationActivity::class.java)
+            startActivity(i)
+        }
     }
 }
